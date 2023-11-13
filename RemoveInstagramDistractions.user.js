a69325dd2a7a16ee8414c00de12701345ef0fe81
Gabriele Bologna <bolognagabriele2003@gmail.com>
2023-11-13 06:11:15 +0100

    Update RemoveInstagramDistractions.user.js

--- RemoveInstagramDistractions.user.js
+++ RemoveInstagramDistractions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram Remove Distractions
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Remove all the Instagram distractions to get concentrated on what's really important
 // @author       GabryB03
 // @match        https://www.instagram.com/*
@@ -14,6 +14,16 @@
 (function()
 {
     'use strict';
+    const css =
+    `
+        a[href='/explore/'], a[href='/reels/'], a[href='https://www.threads.net/'], footer[role='contentinfo'] { display: none !important; visibility: hidden !important; }
+    `;
+    const head = document.head || document.getElementsByTagName('head')[0];
+    const styleElement = document.createElement('style');
+    styleElement.type = 'text/css';
+    styleElement.innerHTML = css;
+    head.appendChild(styleElement);
+
 
     function getElementByXpath(path)
     {
@@ -38,39 +48,14 @@
                         feedElement.remove();
                     }
                 }
-    
-                {
-                    var distractingElements = ["/explore/", "/reels/", "https://www.threads.net/"];
-    
-                    for (var i = 0; i < distractingElements.length; i++)
-                    {
-                        var distractingElement = document.querySelector("a[href='" + distractingElements[i] + "']");
-    
-                        if (distractingElement != null && distractingElement != undefined)
-                        {
-                            distractingElement.remove();
-                        }
-                    }
-                }
-    
-                {
-                    var footerElement = document.querySelector("footer[role='contentinfo']");
-    
-                    if (footerElement != null && footerElement != undefined)
-                    {
-                        footerElement.remove();
-                    }
-                }
 
-                {
-                    var notificationsElement = getElementByXpath("/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[6]");
+                var notificationsElement = getElementByXpath("/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[6]");
 
-                    if (notificationsElement != null && notificationsElement != undefined)
+                if (notificationsElement != null && notificationsElement != undefined)
+                {
+                    if (notificationsElement.innerHTML.includes("M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"))
                     {
-                        if (notificationsElement.innerHTML.includes("M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"))
-                        {
-                            notificationsElement.remove();
-                        }
+                        notificationsElement.remove();
                     }
                 }
             }
